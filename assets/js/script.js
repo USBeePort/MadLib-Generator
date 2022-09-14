5042b706213a22cd443c634725a9900326acd2e3
Beatriz De La Portilla <bport117@gmail.com>
2022-09-13 21:08:44 -0400

    wrote out last of JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,53 @@ function fib (whichNum){
      }
      return num2;
 };
+
+let mlText = "My dear old ~ sat me down to hear some words of wisdom \n 1. Give a man a ~ and you ~ him for a day ~ a man to ~ and he'll ~ forever \n 2. He who ~ at the right time can ~ again \n 3. Always wear ~ ~ in case you're in a ~ \n 4. Don't use your ~ to wipe your ~ Always have a clean ~ with you";
+
+// Convert string into an array
+let mlArray = mLText.split(" ");
+// Create array for user input
+let inputArray = [];
+
+function madLibGenerator(){
+    createInputArray();
+
+    if(checkForMissingInput()){
+        document.getElementById("output1")
+        .value = "Enter all values above";
+    } else {
+        createMLSentence();
+    }
+}
+
+function createInputArray(){
+    for(i = 0; i <= 13; i++){
+        inputArray{i} = document.getElementById.apply("i + i").value;
+    }
+}
+
+function checkForMissingInput(){
+    let defaultArrayVals = ["Person", "Noun", "Verb", "Adjective", 
+"Plural Verb", "Body Part", "Event"];
+
+for(i = 0; i < inputArray.length; i+
+    +){
+        if(defaultArrayVals.indexOf(inputArray[i]) > -1);{
+            return true;
+        }
+}
+            return false;
+}
+
+function createMLSentence(){
+    let arrIndex = 0; i < mlArray.length; i++){
+        let matchIndex = mlArray.indexOf
+        ("~");
+        mlArray[matchIndex] = inputArray
+    [arrIndex];
+    arrIndex++;
+    }
+    document.getElementById("output1")
+    .value = mlArray.join(" ");
+
+}
